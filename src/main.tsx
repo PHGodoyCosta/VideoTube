245d4e0d1b069f04ffe70a790c321381c1149a70
phgodoycosta <phgodoycosta@gmail.com>
2024-11-27 22:17:50 -0400

    Mais uma parte do desingn na página de vídeo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
-import './assets/css/bootstrap.min.css'
+//import './assets/css/bootstrap.min.css'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/pages/Video/VideoPage.tsx
+++ src/pages/Video/VideoPage.tsx
@@ -5,6 +5,7 @@ import MenuGeral from "../Home/Components/MenuGeral/MenuGeral"
 import styles from './VideoPage.module.css'
 import video from '../../assets/videos/video.mp4'
 import no_profile from '../../assets/images/noprofile.jpg'
+import { BsHandThumbsUp, BsHandThumbsDown, BsShare } from 'react-icons/bs'
 
 type MainProps = {
     menuDisplay: CSSProperties
@@ -17,8 +18,10 @@ function Main({menuDisplay}: MainProps) {
                 <MenuGeral menuDisplay={menuDisplay} />
                 <div className={styles.main_area}>
                     <div className={styles.central_areas}>
-                        <div style={{width: "80%"}}>
-                            <video src={video} className={styles.main_video} controls></video>
+                        <div style={{width: "80%", maxWidth: "1000px"}}>
+                            <div>
+                                <video src={video} className={styles.main_video} controls></video>
+                            </div>
                             <h1>Eternxlkz - BRODYAGA FUNK (ULTRA SLOWED + REVERB)</h1>
                             <div style={{display: "flex", justifyContent: "space-between"}}>
                                 <div className={styles.profile}>
@@ -30,12 +33,51 @@ function Main({menuDisplay}: MainProps) {
                                             <span>Nome do canal</span>
                                         </div>
                                     </div>
-                                    <button>Inscrever-se</button>
+                                    <button className="premium_button">Inscrever-se</button>
                                 </div>
                                 <div style={{display: "flex", gap: "10px"}}>
-                                    <button className="btn btn-primary">Like</button>
-                                    <button>Deslike</button>
-                                    <button>Compartilhar</button>
+                                    <button className="normal_button">
+                                        <BsHandThumbsUp fontSize={15} />
+                                        <span>Like</span>
+                                    </button>
+                                    <button className="normal_button">
+                                        <BsHandThumbsDown fontSize={15} />
+                                        <span>Deslike</span>
+                                    </button>
+                                    <button className="normal_button">
+                                        <BsShare fontSize={15} />
+                                        <span>Compartilhar</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div style={{marginTop: "20px"}}>
+                                <p><strong>69.100.200.300 visualizações - há 3 dias</strong></p>
+                                <p style={{maxWidth: "80%"}}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolore voluptatum tenetur iure aliquid consequuntur necessitatibus impedit facere corrupti commodi maiores a aliquam, odit, accusamus suscipit distinctio exercitationem voluptatem assumenda!
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum itaque tenetur autem vel nulla excepturi cumque libero animi. Inventore veniam consequuntur autem minima tempora commodi voluptatibus fugit, voluptatum laboriosam?
+                                </p>
+                            </div>
+                            {/* Seção de comentários: */}
+                            <div style={{marginTop: "20px"}}>
+                                <h3>2450 comentários</h3>
+                                <div className={styles.profile} style={{alignItems: "center", justifyContent: "space-between"}}>
+                                    <img src="https://i.pinimg.com/736x/63/65/94/636594ef0e42c7a30cf5d46b49a726f4.jpg" alt="Imagem de perfil" />
+                                    <input type="text" placeholder="Adicione um comentário..." style={{minWidth: "60%", padding: "10px"}} />
+                                    <button className="action_button">Comentar</button>
+                                </div>
+                                <div style={{marginTop: "20px"}}>
+                                    <div className={styles.comentario}>
+                                        <div>
+                                            <img src="https://i.pinimg.com/736x/63/65/94/636594ef0e42c7a30cf5d46b49a726f4.jpg" alt="Imagem de perfil" />
+                                        </div>
+                                        <div>
+                                            <h4>@mugenitachildz</h4>
+                                            <p>
+                                                1980:wow nice song<br/>
+                                                2023:One of the most historical moments in war...
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
